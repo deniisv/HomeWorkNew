689756d8cf967a915de5909519316df1368033a3
deniisv <142237084+deniisv@users.noreply.github.com>
2023-09-22 20:43:32 +0700

    Add files via upload

---
+++ Lesson 76/main.js
@@ -0,0 +1,10 @@
+const firstName = 'Denis' 
+const surname = 'Semenov'
+let age = '26' 
+let weight = '75'
+const height = '177'
+
+console.log (firstName, surname, age, 'y.o')
+console.log (weight, 'kg', height , 'sm')
+
+console.log(('b' + 'a' + + 'a' + 'a').toLowerCase())
---
+++ Lesson 79/main.js
@@ -0,0 +1,16 @@
+/*const age = 26
+
+let result = null
+
+if (age >= 18) {
+    result = 'Вы совершеннолетний'
+    console.log (result)
+} else {
+    result = 'Ваме еще нет 18'
+    console.log (result)
+}   
+*/
+
+const age = 26
+
+age >= 18 ? console.log('Вы совершеннолетний') : console.log('Вам еще нет 18')
